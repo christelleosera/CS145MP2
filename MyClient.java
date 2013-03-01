af03f4a7ea4cfa5b8cedccc4dd61cc793bb8c1f5
cosera <mc.osera@gmail.com>
2013-03-01 20:39:29 +0800

    Create MyClient.java

---
+++ MyClient.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.*;
+
+public class MyClient{
+  public static void main(String args[]) {
+		int points = 250, i;
+		try{
+			Socket s = new Socket("127.0.0.1", 8888);
+			System.out.println("Client connected!");
+			MyConnection conn = new MyConnection(s);
+
+			while(points > 0){
+				System.out.print("Enter message: ");
+				InputStreamReader reader = new InputStreamReader(System.in);
+				BufferedReader read = new BufferedReader(reader);
+				String msgOut = read.readLine();
+				
+				/*CHECKING TO BE DONE BEFORE SENDING:
+					- check if location specified is empty
+					- check if the player has enough points
+					- check if location specified is valid (sa area niya talaga)
+				*/
+				
+				conn.sendMessage(msgOut);
+				
+				String msgIn = conn.getMessage();
+				
+				if(msgIn.charAt(0) == '$'){
+					points = points - Integer.parseInt(msgIn.substring(1));
+					System.out.println("POINTS: " + points);
+				}
+			}
+		} catch (Exception e){}
+		
+	}
+}
+
