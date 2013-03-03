2ebedb11eb6483f8918e5d05ce1fd106cae52717
Jaymelyn Ilao <jaymelyn.ilao@yahoo.com>
2013-03-03 17:59:50 +0800

    Added done[] array.
    Para malaman ni server na tapos na mamili ng characters yung both
    players, para sabay mag-attack.

--- MyServer.java
+++ MyServer.java
@@ -3,31 +3,35 @@ import java.net.*;
 
 
 public class MyServer {
-  public static void main(String args[]) {
-	int playerNum = 1;	
-	int i, j;
-	MyConnection connArray[] = new MyConnection[2];	
-	Character board[][] = new Character[4][12];
 	
-	//initialize the board
-	for(i=0; i<=3; i++){
-		for(j=1; j<=10; j++){
-			board[i][j] = new Character();
+	public static void main(String args[]) {
+		int playerNum = 0;	
+		int i, j;
+		MyConnection connArray[] = new MyConnection[2];	
+		Character board[][] = new Character[4][12];
+		Boolean done[] = new Boolean[2];
+		done[0] = false;
+		done[1] = false;
+		
+		//initialize the board
+		for(i=0; i<=3; i++){
+			for(j=1; j<=10; j++){
+				board[i][j] = new Character();
+			}
 		}
-	}
-	
-	//cupcakes
-	board[0][0] = new Character("cupcake", 0, 300, 0, 0, 0, 0);
-	board[1][0] = new Character("cupcake", 0, 300, 0, 1, 0, 0);
-	board[2][0] = new Character("cupcake", 0, 300, 0, 2, 0, 0);
-	board[3][0] = new Character("cupcake", 0, 300, 0, 3, 0, 0);
-	
-	board[0][11] = new Character("cupcake", 0, 300, 0, 0, 11, 1);
-	board[1][11] = new Character("cupcake", 0, 300, 0, 1, 11, 1);
-	board[2][11] = new Character("cupcake", 0, 300, 0, 2, 11, 1);
-	board[3][11] = new Character("cupcake", 0, 300, 0, 3, 11, 1);
-	
-			
+		
+		//cupcakes
+		board[0][0] = new Character("cupcake", 0, 300, 0, 0, 0, 0);
+		board[1][0] = new Character("cupcake", 0, 300, 0, 1, 0, 0);
+		board[2][0] = new Character("cupcake", 0, 300, 0, 2, 0, 0);
+		board[3][0] = new Character("cupcake", 0, 300, 0, 3, 0, 0);
+		
+		board[0][11] = new Character("cupcake", 0, 300, 0, 0, 11, 1);
+		board[1][11] = new Character("cupcake", 0, 300, 0, 1, 11, 1);
+		board[2][11] = new Character("cupcake", 0, 300, 0, 2, 11, 1);
+		board[3][11] = new Character("cupcake", 0, 300, 0, 3, 11, 1);
+		
+				
 		try {
 			System.out.println("Server: Starting...");
 			ServerSocket ssocket = new ServerSocket(8888);
@@ -39,11 +43,15 @@ public class MyServer {
 				System.out.print("Server: somebody connected!");
 				System.out.println(socket.getInetAddress());
 				//start thread
-				new ServerListener(socket, playerNum, connArray[playerNum], board);
+				ServerListener s = new ServerListener(socket, playerNum, connArray[playerNum], board, done);
+				s.start();
+				
 				playerNum++;
 			}
 
 		} catch (Exception e){ e.printStackTrace(); }
 	}
+	
+	
 }
 
