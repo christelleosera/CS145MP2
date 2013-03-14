09a431437ec69102678ec63d82f3e79382713799
Jaymelyn Ilao <jaymelyn.ilao@yahoo.com>
2013-03-15 00:47:19 +0800

    Edited paint.
    Ready for repaint of board without images for attack.

--- MyClientPanel.java
+++ MyClientPanel.java
@@ -8,7 +8,13 @@ import java.net.URL;
 
 public class MyClientPanel extends JPanel{
 	
-	private Image candycane_p1, chocnut_p1, donut_p1, gingerbread_p1, gummybear_p1, jellybean_p1;
+	/*players*/
+	private static final int PLAYER1 = 0;
+	private static final int PLAYER2 = 1;
+	
+	private Image candycane_p1, chocnut_p1, donut_p1, gingerbread_p1, gummybear_p1, jellybean_p1, cupcake_p1;
+	private Image candycane_p2, chocnut_p2, donut_p2, gingerbread_p2, gummybear_p2, jellybean_p2, cupcake_p2;
+	
 	Character board[][];
 	 
 	private Image background, board_p1, board_p2;
@@ -33,21 +39,116 @@ public class MyClientPanel extends JPanel{
 	            if(url == null) throw new IOException("Couldn't load board_p2.png");
 	            board_p2 = new ImageIcon(url).getImage();
 	            
+	            /*PLAYER 1*/
 	            url = getClass().getResource("images/candycane_p1.png");
 	            if(url == null) throw new IOException("Couldn't load candycane_p1.png");
 	            candycane_p1 = new ImageIcon(url).getImage();
 	            
+	            url = getClass().getResource("images/chocnut_p1.png");
+	            if(url == null) throw new IOException("Couldn't load chocnut_p1.png");
+	            chocnut_p1 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/donut_p1.png");
+	            if(url == null) throw new IOException("Couldn't load donut_p1.png");
+	            donut_p1 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/gingerbreadman_p1.png");
+	            if(url == null) throw new IOException("Couldn't load gingerbreadman_p1.png");
+	            gingerbread_p1 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/gummybear_p1.png");
+	            if(url == null) throw new IOException("Couldn't load gummybear_p1.png");
+	            gummybear_p1 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/jellybean_p1.png");
+	            if(url == null) throw new IOException("Couldn't load jellybean_p1.png");
+	            jellybean_p1 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/cupcake_p1.png");
+	            if(url == null) throw new IOException("Couldn't load cupcake_p1.png");
+	            cupcake_p1 = new ImageIcon(url).getImage();
+	            
+	            /*PLAYER 2*/
+	            url = getClass().getResource("images/candycane_p2.png");
+	            if(url == null) throw new IOException("Couldn't load candycane_p2.png");
+	            candycane_p2 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/chocnut_p2.png");
+	            if(url == null) throw new IOException("Couldn't load chocnut_p2.png");
+	            chocnut_p2 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/donut_p2.png");
+	            if(url == null) throw new IOException("Couldn't load donut_p2.png");
+	            donut_p2 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/gingerbreadman_p2.png");
+	            if(url == null) throw new IOException("Couldn't load gingerbreadman_p2.png");
+	            gingerbread_p2 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/gummybear_p2.png");
+	            if(url == null) throw new IOException("Couldn't load gummybear_p2.png");
+	            gummybear_p2 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/jellybean_p2.png");
+	            if(url == null) throw new IOException("Couldn't load jellybean_p2.png");
+	            jellybean_p2 = new ImageIcon(url).getImage();
+	            
+	            url = getClass().getResource("images/cupcake_p2.png");
+	            if(url == null) throw new IOException("Couldn't load cupcake_p2.png");
+	            cupcake_p2 = new ImageIcon(url).getImage();
 	        } catch(IOException e) {
 	            System.err.println(e);
 	        }
    }
 	
 	public void paintComponent(Graphics g){
+		int i=0, j=0, owner=-1;
+		String name="";
 		super.paintComponent(g);	
 		
 		g.drawImage(background, 0, 0, null);
 		g.drawImage(board_p1, 50, 100, null);
 		g.drawImage(board_p2, 550, 100, null);
+		for(i=0; i<4; i++){
+			for(j=0; j<10; j++){
+				owner = board[i][j].owner;
+				name = board[i][j].name;
+				System.out.println("owner: " + owner + " name: " + name);
+				if(owner == PLAYER1){
+					if(name.equals("cupcake"))
+						g.drawImage(cupcake_p1, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("canycane"))
+						g.drawImage(candycane_p1, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("chocnut"))
+						g.drawImage(chocnut_p1, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("donut"))
+						g.drawImage(donut_p1, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("gingerbread"))
+						g.drawImage(gingerbread_p1, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("gummybear"))
+						g.drawImage(gummybear_p1, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("jellybean"))
+						g.drawImage(jellybean_p1, 50+(j*100), 100+(i*100), null);
+				}
+				else if(owner == PLAYER2){
+					if(name.equals("cupcake"))
+						g.drawImage(cupcake_p2, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("canycane"))
+						g.drawImage(candycane_p2, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("chocnut"))
+						g.drawImage(chocnut_p2, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("donut"))
+						g.drawImage(donut_p2, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("gingerbread"))
+						g.drawImage(gingerbread_p2, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("gummybear"))
+						g.drawImage(gummybear_p2, 50+(j*100), 100+(i*100), null);
+					else if(name.equals("jellybean"))
+						g.drawImage(jellybean_p2, 50+(j*100), 100+(i*100), null);
+				}
+			}
+		}
+		
 		
 	}
 
